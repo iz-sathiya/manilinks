9d2b06507786771323e0b629c409d972660d867b
Mubarak Cc7 <mubarakcc7@gmail.com>
2017-02-03 20:56:13 +0530

    #side-bar-Navigation, #site-info-sec

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -63,9 +63,19 @@ $(function(){
 
 
     // $('.tester > div').matchHeight();
+       $('.row-plate .single-info').matchHeight();
+
+    // sidebar menu
+       var sideslider = $('[data-toggle=collapse-side]');
+        var sel = sideslider.attr('data-target');
+        var sel2 = sideslider.attr('data-target-2');
+        sideslider.click(function(event){
+            $(sel).toggleClass('in');
+            $(sel2).toggleClass('out');
+        });
     
 
-});
+}); // Ready Function
 
 
 // Resize Function 
